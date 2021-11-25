fbb7e1b0c4c5ce506e2d8adfbbe3db5eeb94a327
Francisco Di Lazaro <47666775+FQuintas@users.noreply.github.com>
2021-11-25 10:02:26 -0300

    Add files via upload

---
+++ Scripts/Transacional/povoamento_fornecedor.sql
@@ -0,0 +1,32 @@
+
+use brinquedos;
+
+select * from produto;
+
+select * from fornecedor;
+
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (1, 'meus Brinquedos ltda', 'max steel');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (2, 'meus Brinquedos ltda', 'barbie 1');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (3, 'meus Brinquedos ltda', 'barbie 2');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (4, 'meus Brinquedos ltda', 'barbie 3');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (5, 'meus Brinquedos ltda', 'barbie 4');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (12, 'meus Brinquedos ltda', 'Patinete max steel');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (13, 'meus Brinquedos ltda', 'Patinete Barbie');
+
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (6, 'Brinquedos paratodos', 'Pepa pig');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (7, 'Brinquedos paratodos', 'Funko pop Shrek');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (8, 'Brinquedos paratodos', 'Ursinho de pelucia Seu amigao');
+
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (14, 'Fornecedor Sla ltda', 'Bicicleta infantil tipo 1');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (15, 'Fornecedor Sla ltda', 'Bicicleta infantil tipo 2');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (16, 'Fornecedor Sla ltda', 'Cavalo de pelucia Sla');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (17, 'Fornecedor Sla ltda', 'alguma coisa de pelucia');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (18, 'Fornecedor Sla ltda', 'WAR');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (19, 'Fornecedor Sla ltda', 'UNO');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (20, 'Fornecedor Sla ltda', 'Quebra cabeca 1000 pecas');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (21, 'Fornecedor Sla ltda', 'Banco Imobiliario');
+
+
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (22, 'Fornecedor  de comida ? Sla ltda', 'Pote de maizena');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (23, 'Fornecedor Sla ltda', 'Amoeba 1');
+insert into fornecedor(cod_fornecedor, nm_fornecedor, prod_fornecido) values (24, 'Fornecedor Sla ltda', 'Amoeba 2');
